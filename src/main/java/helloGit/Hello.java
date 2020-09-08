05214b3674a9dcefbacdd4e53cdbf41afee36b9f
songguangcheng <1138810467@qq.com>
2020-09-08 17:28:16 +0800

    切换分支

--- src/main/java/helloGit/Hello.java
+++ src/main/java/helloGit/Hello.java
@@ -5,5 +5,8 @@ public class Hello {
 		System.out.println("hello Git!");
 		System.out.println("更新1");
 		System.out.println("更新2");
+		System.out.println("创建分支");
+		System.out.println("合并分支");
+		System.out.println("分支咋一样啊");
 	}
 }
